09733db1c39abc1fdf92e7df32f6896cd5b99a28
makoks <kolesoks@gmail.com>
2022-06-27 00:07:28 +0500

    feat(Layout): Add header and menu icons

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import { Layout as AntdLayout, Menu } from 'antd';
-import { Link, Outlet, Navigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Layout as AntdLayout, Menu, Space, Typography, Badge, Avatar, Button } from 'antd';
+import { Link, Outlet, Navigate, useNavigate } from 'react-router-dom';
+import { BellOutlined, LogoutOutlined, ProfileOutlined, UserOutlined, DashboardOutlined, ShopOutlined } from '@ant-design/icons';
+import { Logo } from '../images';
 import { useAuth, useCurrentPage } from '../hooks';
+import { API } from '../API';
 
 export const Layout = () => {
   const currentPage = useCurrentPage();
   const discordToken = useAuth();
+
+  const [user, setUser] = useState({});
+
+  const navigate = useNavigate();
+  const onLogoutHandler = () => {
+    localStorage.removeItem('discord_token');
+    navigate('login');
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      setUser(await API.user());
+    };
+
+    fetchUser();
+  }, []);
 
   if (!discordToken) {
     return <Navigate to="login" />;
@@ -13,14 +32,35 @@ export const Layout = () => {
 
   return (
     <AntdLayout style={{ minHeight: '100vh' }}>
-      <AntdLayout.Header style={{ color: '#fff' }}>Личный кабинет</AntdLayout.Header>
+      <AntdLayout.Header style={{ color: '#fff', display: 'flex', justifyContent: 'space-between' }}>
+        <Space size="middle">
+          <img src={Logo} alt="logo" width={32} height={24} />
+          <Typography.Text strong style={{ color: '#fff', fontSize: 18 }}>
+            Личный кабинет
+          </Typography.Text>
+        </Space>
+        <Space size="large">
+          <Badge count={11}>
+            <Link to="profile">
+              <BellOutlined style={{ color: '#fff', fontSize: 25 }} />
+            </Link>
+          </Badge>
+          <Space>
+            <Avatar src={user.avatarUrl} size={30} />
+            {user.name?.slice(0, user.name.indexOf(' ') + 2)}
+          </Space>
+          <Button type="link" onClick={onLogoutHandler} style={{ fontSize: 20, color: '#fff' }}>
+            <LogoutOutlined />
+          </Button>
+        </Space>
+      </AntdLayout.Header>
       <AntdLayout>
         <AntdLayout.Sider style={{ boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.15)' }}>
           <Menu mode="inline" style={{ height: '100%', borderRight: 0 }} selectedKeys={[currentPage]} items={[
-            { label: <Link to="/">Рубрики</Link>, key: 'rubrics' },
-            { label: <Link to="profile">Профиль</Link>, key: 'profile' },
-            { label: <Link to="vacation">Отпуск</Link>, key: 'vacation' },
-            { label: <Link to="shop">Магазин</Link>, key: 'shop' },
+            { label: <Link to="/">Рубрики</Link>, key: 'rubrics', icon: <ProfileOutlined /> },
+            { label: <Link to="profile">Профиль</Link>, key: 'profile', icon: <UserOutlined /> },
+            { label: <Link to="vacation">Отпуск</Link>, key: 'vacation', icon: <DashboardOutlined /> },
+            { label: <Link to="shop">Магазин</Link>, key: 'shop', icon: <ShopOutlined /> },
           ]} />
         </AntdLayout.Sider>
         <Outlet />
